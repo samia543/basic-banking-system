3c5d1b958ee6fbae8ee6e468a743217070b7aa1b
Samia Mahreen <83362098+samia543@users.noreply.github.com>
2021-07-14 20:13:30 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$conn = mysqli_connect('shareddb-x.hosting.stackcp.net','samia','12345678@','malyaraaj-313537e38d');
-
-	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-	}
-
-?>
